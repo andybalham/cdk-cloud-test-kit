d70becb018e5f6f609b17753394e3fc0773b4500
Andy Blackledge <andyblackledge@hotmail.com>
2023-05-14 17:15:13 +0100

    Updated S3

--- examples/examplesApp.ts
+++ examples/examplesApp.ts
@@ -5,10 +5,12 @@ import NotificationHubTestStack from './notification-hub/NotificationHubTestStac
 import RequestApiTestStack from './request-api/RequestApiTestStack';
 import SimpleEventRouterTestStack from './simple-event-router/SimpleEventRouterTestStack';
 import SimpleMessageRouterTestStack from './simple-message-router/SimpleMessageRouterTestStack';
+import TempTestStack from './TempStack';
 
 const app = new cdk.App();
 cdk.Tags.of(app).add('app', 'ExamplesApp');
 
+new TempTestStack(app, TempTestStack.Id);
 new SimpleEventRouterTestStack(app, SimpleEventRouterTestStack.Id);
 new SimpleMessageRouterTestStack(app, SimpleMessageRouterTestStack.Id);
 new LoanProcessorTestStack(app, LoanProcessorTestStack.Id);
--- src/S3TestClient.ts
+++ src/S3TestClient.ts
@@ -1,24 +1,29 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import AWS from 'aws-sdk';
-import { clearAllObjects } from './s3';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { clearAllObjects, getObjectStringAsync } from './s3';
 
 export default class S3TestClient {
   //
-  readonly s3: AWS.S3;
+  readonly s3Client: S3Client;
 
   constructor(public readonly region: string, public readonly bucketName: string) {
-    this.s3 = new AWS.S3({ region });
+    this.s3Client = new S3Client({ region });
   }
 
   async uploadObjectAsync(key: string, object: Record<string, any>): Promise<void> {
     //
-    await this.s3
-      .upload({
+    await this.s3Client.send(
+      new PutObjectCommand({
         Bucket: this.bucketName,
         Key: key,
         Body: JSON.stringify(object),
       })
-      .promise();
+    );
+  }
+
+  async getObjectAsync(key: string): Promise<Record<string, any> | undefined> {
+    const objectString = await getObjectStringAsync(this.region, this.bucketName, key);
+    return objectString ? JSON.parse(objectString) : undefined;
   }
 
   async clearAllObjectsAsync(prefix?: string): Promise<void> {
